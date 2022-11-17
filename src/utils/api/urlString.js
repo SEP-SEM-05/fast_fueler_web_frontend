3f0f0dacb39debe6a4f3f225ef4f4637445e087b
Kalana99 <kalanarajika99@gmail.com>
2022-11-17 11:06:27 +0530

    local url str commented FE check 03

--- src/utils/api/urlString.js
+++ src/utils/api/urlString.js
@@ -1,4 +1,4 @@
-const local_url = "http://localhost:5000/"; //local host server URL
+// const local_url = "http://localhost:5000/"; //local host server URL
 const deployed_url = "http://fastfuelerbackend-env.eba-ueymd2ax.us-east-1.elasticbeanstalk.com/";
 
 // let url = local_url;
