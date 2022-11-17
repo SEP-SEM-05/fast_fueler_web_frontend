12ed95620b0c8a7a371c31bc07e1c960a88535da
Kalana99 <kalanarajika99@gmail.com>
2022-11-17 11:29:06 +0530

    https applied to api calls FE check 04

--- src/utils/api/urlString.js
+++ src/utils/api/urlString.js
@@ -1,5 +1,5 @@
 // const local_url = "http://localhost:5000/"; //local host server URL
-const deployed_url = "http://fastfuelerbackend-env.eba-ueymd2ax.us-east-1.elasticbeanstalk.com/";
+const deployed_url = "https://fastfuelerbackend-env.eba-ueymd2ax.us-east-1.elasticbeanstalk.com/";
 
 // let url = local_url;
 let url = deployed_url;
